44dbd272d6111402ff05e361b5e5ff51e0f0b160
victornmeka <nme22001@byui.edu>
2023-11-12 15:34:18 +0100

    added remove from cart feature

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,15 +2,25 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  
+
   if (cartItems !== null) {
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
     document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+    // Add event listeners to the remove buttons (X icons)
+    const removeButtons = document.querySelectorAll('.remove-item');
+    removeButtons.forEach(button => {
+      button.addEventListener('click', function () {
+        const productId = this.getAttribute('data-id');
+        removeFromCart(productId);
+      });
+    });
   }
 }
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
+  const newItem = `<li class="cart-card divider" data-id="${item.ProductId}">
+  <span class="remove-item" data-id="${item.ProductId}">X</span>
   <a href="#" class="cart-card__image">
     <img
       src="${item.Image}"
@@ -26,6 +36,20 @@ function cartItemTemplate(item) {
 </li>`;
 
   return newItem;
+}
+
+function removeFromCart(productId) {
+  const cartItems = getLocalStorage("so-cart");
+
+  if (cartItems !== null) {
+    const updatedCart = cartItems.filter(item => item.ProductId !== productId);
+
+    // Update the cart in local storage
+    localStorage.setItem("so-cart", JSON.stringify(updatedCart));
+
+    // Re-render the cart
+    renderCartContents();
+  }
 }
 
 renderCartContents();
