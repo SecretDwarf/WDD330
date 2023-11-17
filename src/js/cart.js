bc07370e15c9debfee1863692c43fd2f6d689da8
Trevor <ngw21001@byui.edu>
2023-11-17 19:11:04 +0200

    team03

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,47 +2,27 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  const productList = document.querySelector(".product-list");
-
-  if (cartItems !== null) {
-    const htmlItems = cartItems.map((item) => cartItemTemplate(item)).join("");
-    productList.innerHTML = htmlItems;
-
-    const removeButtons = document.querySelectorAll('.remove-item');
-    removeButtons.forEach(button => {
-      button.addEventListener('click', function () {
-        const productId = this.getAttribute('data-id');
-        removeFromCart(productId);
-      });
-    });
-  }
-  }
-
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider" data-id="${item.ProductId}">
-    <span class="remove-item" data-id="${item.ProductId}">X</span>
-    <a href="#" class="cart-card__image">
-      <img src="${item.Image}" alt="${item.Name}" />
-    </a>
-    <a href="#">
-      <h2 class="card__name">${item.Name}</h2>
-    </a>
-    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-    <p class="cart-card__quantity">qty: 1</p>
-    <p class="cart-card__price">$${item.FinalPrice}</p>
-  </li>`;
-
-  return newItem;
+  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 
-function removeFromCart(productId) {
-  const cartItems = getLocalStorage("so-cart");
+function cartItemTemplate(item) {
+  const newItem = `<li class="cart-card divider">
+  <a href="#" class="cart-card__image">
+    <img
+      src="${item.Image}"
+      alt="${item.Name}"
+    />
+  </a>
+  <a href="#">
+    <h2 class="card__name">${item.Name}</h2>
+  </a>
+  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__price">$${item.FinalPrice}</p>
+</li>`;
 
-  if (cartItems !== null) {
-    const updatedCart = cartItems.filter(item => item.ProductId !== productId);
-    localStorage.setItem("so-cart", JSON.stringify(updatedCart));
-    renderCartContents();
-  }
+  return newItem;
 }
 
 renderCartContents();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -2,8 +2,6 @@
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
 }
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
@@ -13,6 +11,56 @@ export function getLocalStorage(key) {
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+
+// helper to get parameter strings
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product;
+}
+
+
+export function renderWithTemplate(template, parent, data, callback) {
+  parentElement.insertAdjacentHTML("afterbegin", template);
+  if(callback) {
+    callback(data);
+  }
+}
+
+/*(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+) {
+  const htmlStrings = list.map(templateFn);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}*/
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+
+  const headerElement = document.querySelector("#main-header");
+  const footerElement = document.querySelector("main-footer");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
+}
+
+export async function loadTemplate(path) {
+  const html = await fetch(path).then(convertToText);
+  const template = document.createElement('template');
+  template.innerHTML = html;
+  return template;
+}
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
